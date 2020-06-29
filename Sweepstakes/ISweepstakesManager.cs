6ebce2b939f5bc23111afabb857d8c656b15e9b0
melaniemozee@gmail.com <melaniemozee@gmail.com>
2020-06-29 14:00:57 -0500

    Created method  and ISweepstakesManager  class

--- Sweepstakes/ISweepstakesManager.cs
+++ Sweepstakes/ISweepstakesManager.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Sweepstakes
 {
-    class ISweepstakesManager
+    public interface ISweepstakesManager
     {
+         void InsertSweepstakes(Sweepstakes sweepstakes);
+         Sweepstakes GetSweepstakes();
+        
+
+       
     }
 }
